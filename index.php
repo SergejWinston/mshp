ac871fc8ee050af224ae30ce5a4767ac762e5385
SergejWinston <sergey.winston@bk.ru>
2023-12-17 18:32:01 +0300

    test

---
+++ index.php
@@ -0,0 +1,86 @@
+<html>
+
+<head>
+    <script src="/vddosw3data.js"></script>
+</head>
+
+<body>
+    <div>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="UTF-8">
+        <title>Check your browser...</title>
+        <link rel="stylesheet" href="https://i.cdnreact.xyz/styl.css">
+        <link rel="stylesheet" href="https://i.cdnreact.xyz/font.css">
+        <div class="react-loader">
+            <div class="react-loader__left-gradient-wrapper">
+                <div class="react-loader__left-small-gradient"></div>
+                <div class="react-loader__left-big-gradient"></div>
+            </div>
+            <div class="react-loader__right-gradient-wrapper">
+                <div class="react-loader__right-big-gradient"></div>
+                <div class="react-loader__right-small-gradient"></div>
+            </div>
+            <div class="react-loader__main-information">
+                <p class="react-loader__title">Обрабатываем ваш запрос,<br>пожалуйста подождите ...</p>
+                <p class="react-loader__description">Все отлично, сейчас мы направим вас на сайт. <img
+                        class="react-loader__description-smile" src="https://i.cdnreact.xyz/19.png"></p>
+            </div> <a class="react-loader__copyright" href="https://www.react.su/" target="_blank">DDoS protection by
+                React <img class="react-loader__copyright-smile" src="https://i.cdnreact.xyz/7.png"></a>
+            <script src="/srcc.js" <="" script="">
+            < /div> </body > < /html>
+            </script>
+        </div>
+    </div>
+    <noscript>
+        <h1 style="text-align:center;color:red;"><strong>Please turn JavaScript on and reload the page.</strong></h1>
+    </noscript>
+    <script>
+    w3IncludeHTML(true);
+    </script>
+    <script type="text/javascript" src="/aes.min.js"></script>
+    <script>
+    var _0x8c53 = ["\x70\x75\x73\x68", "\x72\x65\x70\x6C\x61\x63\x65", "\x6C\x65\x6E\x67\x74\x68",
+        "\x63\x6F\x6E\x73\x74\x72\x75\x63\x74\x6F\x72", "", "\x30", "\x74\x6F\x4C\x6F\x77\x65\x72\x43\x61\x73\x65",
+        "68ec53f77d8300afee56603aefb8add9", "3b107c7d14d7c55930f9fa054b4e3555", "4a173d3203c464ce0d0d3a76ea9be43b",
+        "\x63\x6F\x6F\x6B\x69\x65", "\x52\x33\x41\x43\x54\x4C\x42\x50\x52\x4F\x54\x45\x43\x54\x3D",
+        "\x64\x65\x63\x72\x79\x70\x74",
+        "\x3B\x20\x65\x78\x70\x69\x72\x65\x73\x3D\x54\x68\x75\x2C\x20\x33\x31\x2D\x44\x65\x63\x2D\x33\x37\x20\x32\x33\x3A\x35\x35\x3A\x35\x35\x20\x47\x4D\x54\x3B\x20\x70\x61\x74\x68\x3D\x2F"
+    ];
+
+    function toNumbers(_0xd3dbx2) {
+        var _0xd3dbx3 = [];
+        _0xd3dbx2[_0x8c53[1]](/(..)/g, function(_0xd3dbx2) {
+            _0xd3dbx3[_0x8c53[0]](parseInt(_0xd3dbx2, 16))
+        });
+        return _0xd3dbx3
+    }
+
+    function toHex() {
+        for (var _0xd3dbx2 = 1 == arguments[_0x8c53[2]] && arguments[0][_0x8c53[3]] == Array ? arguments[0] : arguments,
+                _0xd3dbx3 = _0x8c53[4], _0xd3dbx5 = 0; _0xd3dbx5 < _0xd3dbx2[_0x8c53[2]]; _0xd3dbx5++) {
+            _0xd3dbx3 += (16 > _0xd3dbx2[_0xd3dbx5] ? _0x8c53[5] : _0x8c53[4]) + _0xd3dbx2[_0xd3dbx5].toString(16)
+        };
+        return _0xd3dbx3[_0x8c53[6]]()
+    }
+    var a = toNumbers(_0x8c53[7]),
+        b = toNumbers(_0x8c53[8]),
+        c = toNumbers(_0x8c53[9]);
+    document[_0x8c53[10]] = _0x8c53[11] + toHex(slowAES[_0x8c53[12]](c, 2, a, b)) + _0x8c53[13];
+    </script>
+    <center>
+        <br>
+        <br>
+        <center>
+
+        </center>
+    </center>
+    <div at-magnifier-wrapper="">
+        <div class="at-theme-light">
+            <div class="at-base notranslate" translate="no">
+                <div class="EuwGd" style="top: 0px; left: 0px;"></div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
